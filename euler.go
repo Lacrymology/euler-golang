a4dc8bfc37bf771d3305475b840b8db5ef30afb1
Tomas Neme <lacrymology@gmail.com>
2017-12-07 22:10:04 +0100

    main function prints exercises

---
+++ euler.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Lacrymology/euler-golang/euler"
+)
+
+func main () {
+	fmt.Println("Ex001:", euler.Ex001())
+	fmt.Println("Ex002:", euler.Ex002())
+	fmt.Println("Ex003:", euler.Ex003())
+}
