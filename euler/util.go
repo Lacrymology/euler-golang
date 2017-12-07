fa6f10c7f95a36ad01846607b2990c398b987e34
Tomas Neme <lacrymology@gmail.com>
2017-12-07 22:09:29 +0100

    List of primes smaller than n

--- euler/util.go
+++ euler/util.go
@@ -17,3 +17,27 @@ func FilterInts(source []int, f func (int) bool) []int {
 	}
 	return dest
 }
+
+func Primes(limit int) []int {
+	composites := make(map[int]bool)
+	primes := make([]int, 0)
+	p := 2
+
+	for p < limit {
+		// p is a prime
+		primes = append(primes, p)
+
+		// all multiples of p are not primes
+		for i := 2 ; i * p < limit ; i++ {
+			composites[i*p] = true
+		}
+
+		// find the next number not yet marked as a composite
+		p++
+		for present := composites[p] ; present && p < limit ; present = composites[p] {
+			p++
+		}
+	}
+
+	return primes
+}
